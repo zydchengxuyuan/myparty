cafdcd936888e7f484336424032c9e95d21716d4
zydchengxuyuan <1002872775@qq.com>
2020-02-09 23:50:18 +0800

    main.js config

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -11,17 +10,3 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
-=======
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
-Vue.config.productionTip = false
-
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
->>>>>>> 28510c73b2c0f274ded9c433326a24e8d7dc357e
